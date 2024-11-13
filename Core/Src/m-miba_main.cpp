2055bdd200b1f9a69e39aa01442589951a3e6833
Ethan Chun <ethan.l.chun@gmail.com>
2024-11-13 15:43:07 +0000

    Clean up main

--- Core/Src/m-miba_main.cpp
+++ Core/Src/m-miba_main.cpp
@@ -19,6 +19,23 @@ void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim) {
   }
 }
 
+/**
+ * @brief  The application entry point.
+ *
+ * The data format is:
+ * 80 bytes
+ * [--- pressure (72 bytes) ---][--- time (4 bytes) ---][--- eol (4 bytes) ---]
+ * pressure:
+ * 		Every two values is a uint16 pressure reading
+ * 		If p is in pascals, the transmitted value is
+ * 		p - P_MIN where P_MIN = 70000pa.  This ensures
+ * 		that we can represent all values with uint16
+ * time:
+ *  	This is a single uint32 value for the sample loop time
+ * eol:
+ *  	0xFFFFFFFF
+ *
+ */
 int mmiba_main(void) {
   printf("Hello World!\n\r");
   CoreDebug->DEMCR |= CoreDebug_DEMCR_TRCENA_Msk;  // Enable DWT access
@@ -51,105 +68,52 @@ int mmiba_main(void) {
     HAL_Delay(100);
   }
 
-  uint32_t previousTime = 0;
-  uint32_t currentTime = 0;
   uint32_t elapsedTime = 0;
-  uint32_t elapsedTimeNoPrint = 0;
-  uint32_t sample_start_time = 0;
-
-  uint8_t first_byte;
-  uint8_t second_byte;
   uint16_t sample_data;
-
-  // Interrupt init before setup
-  HAL_TIM_Base_Start_IT(&htim5);
-
-  //	int data_arr[num_sensors];
 
   uint8_t data_buffer[num_sensors * 2];
   uint8_t eol[4] = {0xFF, 0xFF, 0xFF, 0xFF};
   uint8_t time_buffer[4];
 
+  // Interrupt init before setup
+  HAL_TIM_Base_Start_IT(&htim5);
+
   while (1) {
     if (INTERRUPT_FLAG) {
       INTERRUPT_FLAG = 0;
 
-      // Sampling time is ~2ms (2030)
-      // Prining is ~3ms rn
+      // Sampling time is ~2000us (2030)
+      // Printing is ~700us rn
+      // Loop time is set in the ioc by the interrupt timer
+      // period.
 
-      //	  char data_str[100];
-      //	  sample_start_time = __HAL_TIM_GET_COUNTER(&htim4);
+      // Read sensors
       for (int i = 0; i < num_sensors; ++i) {
         sensorList[i].Sample();
-
-        //			  HAL_UART_Transmit(&huart3, (uint8_t *)&ch, 1,
-        //HAL_MAX_DELAY);
-
-        //			  printf("%d,", sensorList[i].raw_data[0]);
-        //		  HAL_UART_Transmit(&huart3, (uint8_t*)
-        //&sensorList[i].raw_data[0], 1, HAL_MAX_DELAY);
-
-        //			  uint16_t data_uint16_t = (uint16_t)
-        //sensorList[i].raw_data[0]; 			  data_str += (char*)
-        //sensorList[i].raw_data[0]; 			  printf("aa");
-        sample_data =
-            sensorList[i].raw_data[0];  // Will not work for negative numbers
-        data_buffer[2 * i] =
-            ((sample_data >> 8) &
-             0xFF);  // <first_byte><second_byte> = <value in binary>
+        sample_data = sensorList[i].raw_data[0];
+        data_buffer[2 * i] = ((sample_data >> 8) & 0xFF);
         data_buffer[2 * i + 1] = (sample_data & 0xFF);
-
-        //			  printf("%c%c", first_byte, second_byte);
       }
 
-      //		  printf("%x", data_buffer[0]);
-      //		  __HAL_TIM_SET_COUNTER(&htim4, 0);
-
-      // Try print with HAL_UAR_TRANSMIT
+      // Send data buffer
       HAL_UART_Transmit(&huart3, data_buffer, num_sensors * 2,
                         HAL_MAX_DELAY);  // This was 779
-                                         //		  printf("%s", data_buffer);
 
-      // Try with printf
-      //		  for (int i = 0; i < num_sensors; i++) {  // 301
-      //			  uint8_t first_part = data_buffer[2 * i];
-      //			  uint8_t second_part = data_buffer[2 * i + 1];
-
-      //			  printf("%c%c", first_part, second_part);
-      //			  HAL_UART_Transmit(&huart3, &first_part, 1,
-      //HAL_MAX_DELAY); 			  HAL_UART_Transmit(&huart3, &second_part, 1,
-      //HAL_MAX_DELAY);
-
-      //		  }
-
-      //	  currentTime = HAL_GetTick();
-      //	  elapsedTime = currentTime - previousTime;
-      //	  previousTime = currentTime;
+      // Calculate elapsed time
       elapsedTime = __HAL_TIM_GET_COUNTER(&htim4);
       __HAL_TIM_SET_COUNTER(&htim4, 0);
 
-      //	  elapsedTimeNoPrint = elapsedTime - sample_start_time;
-
-      //		  printf("%d\n\r", (int) elapsedTime);
-      //		  printf("\n");
-
-      //		  printf("%d", (int) elapsedTime);
+      // Pack time buffer
       time_buffer[0] = (elapsedTime >> 8 * 3) & 0xFF;
       time_buffer[1] = (elapsedTime >> 8 * 2) & 0xFF;
       time_buffer[2] = (elapsedTime >> 8 * 1) & 0xFF;
       time_buffer[3] = (elapsedTime) & 0xFF;
 
+      // Transmit time buffer
       HAL_UART_Transmit(&huart3, time_buffer, 4, HAL_MAX_DELAY);
 
+      // Transmit eol buffer
       HAL_UART_Transmit(&huart3, eol, 4, HAL_MAX_DELAY);  // This was 779
-
-      //		  printf("%d\n\r", (int) elapsedTime);
-
-      //	  printf("\n\r");
     }
-
-    /* USER CODE END WHILE */
-
-    /* USER CODE BEGIN 3 */
   }
 }
